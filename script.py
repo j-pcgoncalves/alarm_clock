ccd5d37db1d6d531581f8bb63e9b2a395b9a8ba5
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-02-21 00:31:44 +0000

    solve syntax error

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from playsound import playsound
 
-alarm_time = input(Enter the time of alarm to be set: HH: MM: SS\n)
+alarm_time = input("Enter the time of alarm to be set: HH: MM: SS\n")
 alarm_hour = alarm_time[0:2]
 alarm_minute = alarm_time[3:5]
 alarm_seconds = alarm_time[6:8]
